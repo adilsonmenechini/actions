d0bc1ea58821fa67886dca7037b32b1973b15769
Adilson Menechini <adilsonmenechini@gmail.com>
2020-12-25 22:46:33 -0300

    Terraform scan sec corrigindo

--- main.tf
+++ main.tf
@@ -13,4 +13,33 @@ provider "aws" {
 resource "aws_s3_bucket" "ci" {
   bucket = "criando-bucket-ci-00"
   acl    = "private"
+
+  tags = {
+    Name        = "Bucket terraform"
+    Environment = "Dev"
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket-ci"
+  acl    = "log-delivery-write"
+}
+
+
+resource "aws_kms_key" "mykey" {
+  deletion_window_in_days = 10
 }
