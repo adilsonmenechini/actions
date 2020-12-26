8461ee63ef459062a5f8f0e1ef9aa84a4693d761
Adilson Menechini <adilsonmenechini@gmail.com>
2020-12-25 23:13:09 -0300

    alterado a ordem

--- main.tf
+++ main.tf
@@ -10,6 +10,25 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_kms_key" "key" {
+  enable_key_rotation  = true
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket-ci"
+  acl    = "log-delivery-write"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
 resource "aws_s3_bucket" "ci" {
   bucket = "criando-bucket-ci-00"
   acl    = "private"
@@ -34,22 +53,4 @@ resource "aws_s3_bucket" "ci" {
   }
 }
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket-ci"
-  acl    = "log-delivery-write"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
-
-
-resource "aws_kms_key" "mykey" {
-  enable_key_rotation  = true
-  deletion_window_in_days = 10
-}
